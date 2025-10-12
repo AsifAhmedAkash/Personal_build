b788820961ce8322cf1630e78e114a9209a5e6a2
Asif Ahmed <asifahmedakash32@gmail.com>
2025-10-12 16:19:29 +0600

    v4

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-SpearMania-1.01";
+const cacheName = "DefaultCompany-SpearMania-1";
 const contentToCache = [
     "Build/Personal_build.loader.js",
     "Build/Personal_build.framework.js",
